4f9a1ae0d8a2a927cf75c9939523f4fdef7dcb36
Elizabeth Karst <lkarst@outlook.com>
2018-08-19 18:15:18 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,11 +2,19 @@ def second_challenge
   epic_tragedy = {
     epic_tragedy = {
       montague: {
-        
+        patriarch: {},
+        matriarch: {},
+        hero: {},
+        hero_friends: {}
       },
-      capulet: {}
+      capulet: {
+        patriarch: {},
+        matriarch: {},
+        heroine: {},
+        heroine_friends: {}
+      }
     }
   }
 
-  
+
 end
