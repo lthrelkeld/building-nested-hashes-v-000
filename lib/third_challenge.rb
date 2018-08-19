32e9f27c7d86c77727be69cfecd682b65e11e6a8
Elizabeth Karst <lkarst@outlook.com>
2018-08-19 18:22:43 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,16 +5,16 @@ def third_challenge
           name: "Lord Montague",
           age: "53"
         },
-        matriarch: {
-          name: "Lady Montague",
-          age: "54"
-        },
+        matriarch: {name: "Lady Montague",age: "54"},
         hero: {
           name: "Romeo",
           age: "15",
           status: "alive"
         },
-        hero_friends: []
+        hero_friends: [
+          {name: "Benvolio", age: "17", attitude: "worried"},
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        ]
       },
       capulet: {
         patriarch: {
