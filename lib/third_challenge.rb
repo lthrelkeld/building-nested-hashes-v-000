919a6d0c3835b4266a486670485991766be2030b
Elizabeth Karst <lkarst@outlook.com>
2018-08-19 18:23:08 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,11 +5,8 @@ def third_challenge
           name: "Lord Montague",
           age: "53"
         },
-        matriarch: {name: "Lady Montague",age: "54"},
-        hero: {
-          name: "Romeo",
-          age: "15",
-          status: "alive"
+        matriarch: {name: "Lady Montague", age: "54"},
+        hero: {name: "Romeo", age: "15", status: "alive"
         },
         hero_friends: [
           {name: "Benvolio", age: "17", attitude: "worried"},
