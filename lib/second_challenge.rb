6e808efa1f72b268c421aec4194c4a846497e77e
Elizabeth Karst <lkarst@outlook.com>
2018-08-19 18:14:14 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+    epic_tragedy = {
+      montague: {
+        
+      },
+      capulet: {}
+    }
+  }
 
   
 end
